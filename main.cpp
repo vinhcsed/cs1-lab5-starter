57ae58445c0235a4e8b3abd83911829cd3365a86
Vincent Tran <33761211+VincentVinhTran@users.noreply.github.com>
2025-03-22 10:06:37 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <cstdlib> 
+using namespace std;
 
+// ** REQUIRED FUNCTIONS **
+char userChoice();
+char computerChoice();
+void determineWinner(char, char);
+bool playAgain();
+// ************************
+
+int main() {
+  return 0;
+}
